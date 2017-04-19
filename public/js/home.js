44bab36803697cd7d8d403679197e5c19b93cf72
Jinyang Yu <jy2803@columbia.edu>
2017-04-19 12:08:10 -0400

    Commit

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,16 @@
 /**
  * Created by jinyangyu on 4/19/17.
  */
+Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
+Survey.Survey.cssType = "bootstrap";
+var survey = new Survey.Model({questions:[
+    { type: "rating", name: "satisfaction", title: "How satisfied are you with the Product?",
+        mininumRateDescription: "Not Satisfied", maximumRateDescription: "Completely satisfied" }]
+});
+
+$("#surveyElement").Survey({model:survey});
+var q = survey.getQuestionByName('satisfaction');
+q.mininumRateDescription = 0;
+q.maximumRateDescription = 5;
+survey.render();
 
